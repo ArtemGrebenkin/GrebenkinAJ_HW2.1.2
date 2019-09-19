38bdd6f709ab4ffef8695d30ddd3409a8c9c04b4
Artem Grebenkin <artoisgrebenkin@gmail.com>
2019-09-19 12:23:51 +0600

    Tack 2 is done

--- GrebenkinAJ_HW2.1.2/ViewController.swift
+++ GrebenkinAJ_HW2.1.2/ViewController.swift
@@ -10,11 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redVC: UIView!
+    @IBOutlet weak var yellowVC: UIView!
+    @IBOutlet weak var greenVC: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
+    
+    var views = [UIView]()
+    var indexOfcurrentView = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redVC.backgroundColor = .red
+        yellowVC.backgroundColor = .yellow
+        greenVC.backgroundColor = .green
+        
+        views = [redVC, yellowVC, greenVC]
+        
+        startButton.layer.cornerRadius = startButton.frame.height / 5
+        
+        for vc in views {
+            vc.layer.cornerRadius = redVC.frame.height / 2
+        }
+        turnLightOff()
+  
     }
+    
 
-
+    @IBAction func startButtonPressed() {
+        
+        turnLightOff()
+        if self.indexOfcurrentView == views.count {self.indexOfcurrentView = 0}
+        views[self.indexOfcurrentView].alpha = 1
+        indexOfcurrentView += 1
+        
+        guard self.startButton.title(for: .normal) == "START" else {return}
+        self.startButton.setTitle("NEXT", for: .normal)
+    }
+    
+    
+    func turnLightOff() {
+        for vc in self.views {
+            vc.alpha = 0.3
+        }
+    }
+    
 }
 
